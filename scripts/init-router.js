b8e575b733206c5a4368bfa31102ec6c511dc84c
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-04-30 22:20:20 +0700

    feat: sharding no replica

--- scripts/init-router.js
+++ scripts/init-router.js
@@ -1,6 +1,5 @@
 sh.addShard("rs-shard-01/shard01-a:27017")
-sh.addShard("rs-shard-01/shard01-b:27017")
-sh.addShard("rs-shard-01/shard01-c:27017")
 sh.addShard("rs-shard-02/shard02-a:27017")
-sh.addShard("rs-shard-02/shard02-b:27017")
-sh.addShard("rs-shard-02/shard02-c:27017")
+
+sh.enableSharding("MyDatabase")
+db.adminCommand( { shardCollection: "MyDatabase.users", key: { _id: "hashed" } } )
