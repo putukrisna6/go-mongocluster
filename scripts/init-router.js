da02ce6c8962938140983572e5d8e1b0596e379c
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-04-30 22:21:11 +0700

    fix(config): add sharding to users in config file

--- scripts/init-router.js
+++ scripts/init-router.js
@@ -4,3 +4,6 @@ sh.addShard("rs-shard-01/shard01-c:27017")
 sh.addShard("rs-shard-02/shard02-a:27017")
 sh.addShard("rs-shard-02/shard02-b:27017")
 sh.addShard("rs-shard-02/shard02-c:27017")
+
+sh.enableSharding("MyDatabase")
+db.adminCommand( { shardCollection: "MyDatabase.users", key: { _id: "hashed" } } )
